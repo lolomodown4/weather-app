198891ba75ee518616bfd5114c60c1a2393a36fb
Emmanuel Alcoriza <alcorizaem18@gmail.com>
2024-03-20 14:58:05 +0800

    done with the logic

--- src/App.js
+++ src/App.js
@@ -4,70 +4,52 @@ import Searchbar from "./component/Searchbar";
 import Weather from "./component/Weather";
 
 function App() {
-  const [city, setCity] = useState("london");
+  const [city, setCity] = useState("korea");
   const [error, setError] = useState();
-  const [data, setData] = useState();
-  const [latitude, setLatitude] = useState();
-  const [longitude, setLongitude] = useState();
-  const [actualData, setActualData] = useState();
+  const [data, setData] = useState([]);
+  const [isDataFetched, setIsDataFetched] = useState(false);
 
   useEffect(() => {
-    getData();
+    fetchData();
   }, [city]);
 
-  const getData = async () => {
+  const fetchData = async () => {
     try {
       let response = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=89c46bc426ab3acd9f8c9cd0b5c756a3`
       );
 
       if (!response.ok) {
-        throw Error("Failed to fetch data");
+        throw Error("Failed to get coordinates of the city");
       }
 
       let jsonResponse = await response.json();
 
-      setData(jsonResponse);
+      const { lat, lon } = jsonResponse[0];
 
-      /* call fucntion */
-      getCoordinates();
+      const weatherDataFetch = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,daily&appid=89c46bc426ab3acd9f8c9cd0b5c756a3&units=imperial`
+      );
+
+      if (!weatherDataFetch.ok) {
+        throw Error("Failed to fetch weather data");
+      }
+
+      const weatherData = await weatherDataFetch.json();
+
+      setData(weatherData);
+      setIsDataFetched(true);
     } catch (err) {
       setError(err.message);
       console.log(error);
     }
   };
 
-  const getCoordinates = () => {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].hasOwnProperty("lat") && data[i].hasOwnProperty("lon")) {
-        setLatitude(data[i].lat);
-        setLongitude(data[i].lon);
-        console.log(data[i].lat);
-        console.log(data[i].lon);
-        break;
-      }
-    }
-    fetchData();
-  };
-
-  const fetchData = async () => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,daily&appid=89c46bc426ab3acd9f8c9cd0b5c756a3&units=imperial`
-    );
-    if (!response.ok) {
-      throw Error("Failed to fetch data");
-    }
-
-    let respData = await response.json();
-
-    setActualData(respData);
-  };
-
   return (
     <div className="App">
       <div className="component-holder">
         <Searchbar city={city} setCity={setCity} />
-        <Weather city={city} actualData={actualData} />
+        {isDataFetched && <Weather city={city} data={data} />}
       </div>
     </div>
   );
--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,21 +1,47 @@
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import CloudIcon from "@mui/icons-material/Cloud";
 import AirIcon from "@mui/icons-material/Air";
 import WaterIcon from "@mui/icons-material/Water";
 
-const Weather = ({ city, actualData }) => {
+const Weather = ({ city, data }) => {
+  console.log(data);
+
+  const icon = () => {
+    if (data.current.weather[0].main === "Clear") {
+      return (
+        <WbSunnyIcon
+          sx={{
+            fontSize: 100,
+          }}
+        />
+      );
+    } else if (data.current.weather[0].main === "Rain") {
+      return (
+        <ThunderstormIcon
+          sx={{
+            fontSize: 100,
+          }}
+        />
+      );
+    } else if (data.current.weather[0].main === "Clouds") {
+      return (
+        <CloudIcon
+          sx={{
+            fontSize: 100,
+          }}
+        />
+      );
+    }
+  };
+
   return (
     <div className="weather-display-container">
       <div className="weather-display-grid">
         <div className="icon-details-container">
-          <div className="icon">
-            <WbSunnyIcon
-              sx={{
-                fontSize: 100,
-              }}
-            />
-          </div>
-          <h1> {actualData.current.temp}</h1>
-          <h2>{city}</h2>
+          <div className="icon">{icon()}</div>
+          <h1> {data ? data.current.temp : "{temp data}"}</h1>
+          <h2>{city ? city : "{city name}"}</h2>
         </div>
         <div className="humid-container">
           <div className="humid-icon">
@@ -27,7 +53,7 @@ const Weather = ({ city, actualData }) => {
           </div>
           <div className="humid-details">
             <div>
-              <div>{actualData.current.humidity}</div>
+              <div>{data ? data.current.humidity : "{humidity data}"}</div>
               <div>humidity</div>
             </div>
           </div>
@@ -42,7 +68,7 @@ const Weather = ({ city, actualData }) => {
           </div>
           <div className="windspeed-details">
             <div>
-              <div>{actualData.current.wind_speed}</div>
+              <div>{data ? data.current.wind_speed : "{windspeed data}"}</div>
               <div>windspeed</div>
             </div>
           </div>
